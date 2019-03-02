9396a5711b08c0236013f4c55bd6e4fcf14904a0
Loong <loong@republicprotocol.com>
2019-03-02 16:56:21 +0800

    replica: fix imports

--- replica/state_test.go
+++ replica/state_test.go
@@ -3,8 +3,7 @@ package replica_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	. "github.com/republicprotocol/hyperdrive/replica"
+	. "github.com/renproject/hyperdrive/replica"
 )
 
 var _ = Describe("State", func() {
