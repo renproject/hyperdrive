69b09601846da9090d68942e0c8f8a28c31b9cf0
Loong <loong@republicprotocol.com>
2019-03-02 16:46:14 +0800

    replica: implement empty replica as a supervised task

--- replica/replica.go
+++ replica/replica.go
@@ -1,1 +1,35 @@
 package replica
+
+import (
+	"github.com/renproject/hyperdrive/sig"
+	"github.com/renproject/hyperdrive/supervisor"
+)
+
+type Broadcaster interface {
+	Send(to sig.Signatory, data interface{})
+	Broadcast(data interface{})
+}
+
+type Replica interface {
+}
+
+type replica struct {
+	state        State
+	stateMachine StateMachine
+}
+
+func New(state State, stateMachine StateMachine) Replica {
+	return &replica{
+		state:        state,
+		stateMachine: stateMachine,
+	}
+}
+
+func Run(ctx supervisor.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		}
+	}
+}
