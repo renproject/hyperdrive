308093fffcef92e0c06e5bd19430a60824a11af1
Loong <loong@republicprotocol.com>
2019-03-05 15:38:38 +0800

    sig, sig/ecdsa: infer the signatory from the private key

--- sig/ecdsa/ecdsa.go
+++ sig/ecdsa/ecdsa.go
@@ -2,52 +2,57 @@ package ecdsa
 
 import (
 	"crypto/ecdsa"
+	"crypto/elliptic"
 
-	ethCrypto "github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 	"github.com/renproject/hyperdrive/sig"
 )
 
-// New needs who you are and your private ECDSA key
-func New(whoami sig.Signatory,
-	privKey *ecdsa.PrivateKey) sig.Authenticator {
-	return signer{
-		whoami:  whoami,
+// PubKeyToSignatory returns a `Signatory` using the same conversion that Ethereum uses to extract addresses from public
+// keys.
+func PubKeyToSignatory(pubKey *ecdsa.PublicKey) sig.Signatory {
+	bytes := elliptic.Marshal(secp256k1.S256(), pubKey.X, pubKey.Y)
+	hash := crypto.Keccak256(bytes[1:]) // Keccak256 hash
+	hash = hash[(len(hash) - 20):]      // Take the last 20 bytes
+	data := sig.Signatory{}
+	copy(data[:], hash)
+	return data
+}
+
+type signerVerifier struct {
+	privKey *ecdsa.PrivateKey
+}
+
+func NewFromPrivKey(privKey *ecdsa.PrivateKey) sig.SignerVerifier {
+	return signerVerifier{
 		privKey: privKey,
 	}
 }
 
-// NewRandKey Generates a private key for you
-func NewRandKey(whoami sig.Signatory) (sig.Authenticator, error) {
-	privKey, err := ethCrypto.GenerateKey()
+func NewFromRandom() (sig.SignerVerifier, error) {
+	privKey, err := crypto.GenerateKey()
 	if err != nil {
 		return nil, err
 	}
-
-	return signer{
-		whoami:  whoami,
+	return signerVerifier{
 		privKey: privKey,
 	}, nil
 }
 
-type signer struct {
-	whoami  sig.Signatory
-	privKey *ecdsa.PrivateKey
+func (signerVerifier signerVerifier) Sign(hash sig.Hash) (sig.Signature, error) {
+	sig, err := crypto.Sign(hash[:], signerVerifier.privKey)
+	return sig, err
 }
 
-func (signer signer) Sign(hash sig.Hash) (sig.Signature, sig.Signatory, error) {
-	sig, err := ethCrypto.Sign(hash[:], signer.privKey)
+func (signerVerifier signerVerifier) Signatory() sig.Signatory {
+	return PubKeyToSignatory(&signerVerifier.privKey.PublicKey)
+}
 
+func (signerVerifier signerVerifier) Verify(hash sig.Hash, signature sig.Signature) (sig.Signatory, error) {
+	pubKey, err := crypto.SigToPub(hash[:], signature)
 	if err != nil {
-		return nil, signer.whoami, err
+		return sig.Signatory{}, err
 	}
-
-	return sig, signer.whoami, err
-}
-
-func (signer signer) PubKey() *ecdsa.PublicKey {
-	return &signer.privKey.PublicKey
-}
-
-func (signer signer) Verify(hash sig.Hash, sig sig.Signature) (*ecdsa.PublicKey, error) {
-	return ethCrypto.SigToPub(hash[:], sig)
+	return PubKeyToSignatory(pubKey), nil
 }
