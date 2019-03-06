b4c89e1183d518ff0f28b561559d69a453c671b7
pfalzgraf <pfalzgraf@drmartin.com>
2019-03-06 17:21:50 +1100

    ecdsa, replica: added package level docs; transition: docs and start of interface implementation

--- replica/transition.go
+++ replica/transition.go
@@ -1,3 +1,23 @@
+/*Package replica contains the interface TransitionBuffer and its
+implementation
+
+There are two types of `Transition`, those with a `Height` and those
+that are "immediate". Any "immediate" `Transition`s will be
+`Dequeue`ed first, regardless of the provided `Height`. Otherwise,
+`Dequeue` will return the most relevant `Transition` for the given
+`Height`. For example: you will not get a `PreVoted` if a
+`PreCommitted` was already `Enqueue`ed at that `Height`.
+
+Keep in mind`Transition`s that don't have a `Height` are not pruned.
+For example: if you `Enqueue` a `TimedOut` twice then the next two
+`Dequeue` will return a `TimedOut`. The "immediate" `Transition`s are
+stored in a FIFO queue.
+
+I also assume you will call `Drop` with your current `Height`
+whenever you are done processing all previous `Height`s to prevent
+`TransitionBuffer` from becoming a memory leak.
+
+*/
 package replica
 
 import (
@@ -6,16 +26,21 @@ import (
 	"github.com/renproject/hyperdrive/block"
 )
 
-// A TransitionBuffer is used to temporarily buffer `Transitions` that are not ready to be processed because of the
-// `State`. All `Transitions` are buffered against their respective `Height` and will be dequeued one by one.
+// A TransitionBuffer is used to temporarily buffer `Transitions` that
+// are not ready to be processed because of the `State`. All
+// `Transitions` are buffered against their respective `Height` and
+// will be dequeued one by one.
 type TransitionBuffer interface {
 	Enqueue(transition Transition)
 	Dequeue(height block.Height) (Transition, bool)
+	// Drop everything below the given Height. You should call this
+	// the moment you know everything below the current height is
+	// meaningless.
 	Drop(height block.Height)
 }
 
-// A Transition is an event that transitions a `StateMachine` from one State to another. It is generated externally to
-// the `StateMachine`.
+// A Transition is an event that transitions a `StateMachine` from one
+// State to another. It is generated externally to the `StateMachine`.
 type Transition interface {
 	IsTransition()
 }
@@ -25,7 +50,8 @@ type TimedOut struct {
 	time.Time
 }
 
-// IsTransition implements the `Transition` interface for the `TimedOut` event.
+// IsTransition implements the `Transition` interface for the
+// `TimedOut` event.
 func (timedOut TimedOut) IsTransition() {
 }
 
@@ -34,24 +60,91 @@ type Proposed struct {
 	block.Block
 }
 
-// IsTransition implements the `Transition` interface for the `Proposed` event.
+// IsTransition implements the `Transition` interface for the
+// `Proposed` event.
 func (proposed Proposed) IsTransition() {
 }
 
-// A PreVoted block has been signed and broadcast by another `Replica`.
+// A PreVoted block has been signed and broadcast by another
+// `Replica`.
 type PreVoted struct {
 	block.SignedPreVote
 }
 
-// IsTransition implements the `Transition` interface for the `PreVoted` event.
+// IsTransition implements the `Transition` interface for the
+// `PreVoted` event.
 func (preVoted PreVoted) IsTransition() {
 }
 
-// A PreCommitted polka has been signed and broadcast by another `Replica`.
+// A PreCommitted polka has been signed and broadcast by another
+// `Replica`.
 type PreCommitted struct {
 	block.SignedPreCommit
 }
 
-// IsTransition implements the `Transition` interface for the `PreCommitted` event.
+//TODO: should we have Commited as well?
+
+// IsTransition implements the `Transition` interface for the
+// `PreCommitted` event.
 func (preCommitted PreCommitted) IsTransition() {
+}
+
+//TODO: needs to remove obsolete Transitions
+func (buffer *transitionBuffer) Enqueue(transition Transition) {
+	switch transition := transition.(type) {
+	case Proposed:
+		buffer.buf[transition.Height].enqueue(transition)
+	case PreVoted:
+		buffer.buf[transition.Height].enqueue(transition)
+	case PreCommitted:
+		buffer.buf[transition.Polka.Height].enqueue(transition)
+	}
+}
+
+func (buffer *transitionBuffer) Dequeue(height block.Height) (Transition, bool) {
+	if tran, ok := buffer.immediate.dequeue(); ok {
+		return tran, true
+	}
+	return buffer.buf[height].dequeue()
+}
+
+func (tq *transitionQueue) Drop(height block.Height) {
+	//TODO: think of a nice way to implement this
+}
+
+// The logic behind the buf is to delete the transitionQueue whenever
+// we get a Transition that makes the previous messages obsolete
+type transitionBuffer struct {
+	buf       map[block.Height]*transitionQueue
+	immediate *transitionQueue
+}
+
+func newQueue() *transitionQueue {
+	return &transitionQueue{
+		queue: make([]Transition, 0),
+		end:   0,
+	}
+}
+
+// FIFO queue for `Transition`
+type transitionQueue struct {
+	queue []Transition
+	end   int
+}
+
+func (tq *transitionQueue) enqueue(tran Transition) {
+	if len(tq.queue) == tq.end {
+		tq.queue = append(tq.queue, tran)
+	} else {
+		tq.queue[tq.end] = tran
+	}
+	tq.end++
+}
+
+func (tq *transitionQueue) dequeue() (Transition, bool) {
+	if tq.end == 0 {
+		return nil, false
+	}
+	tq.end--
+	return tq.queue[tq.end], true
 }
