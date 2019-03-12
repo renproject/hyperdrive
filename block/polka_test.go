f92e9204e6b6dd0e8daae6a1dae9610d9fecf1d2
pfalzgraf <pfalzgraf@drmartin.com>
2019-03-12 17:15:23 +1100

    block/polka_test: adds fuzzy test, needs more coverage

--- block/polka_test.go
+++ block/polka_test.go
@@ -1,1 +1,138 @@
 package block_test
+
+import (
+	"math/rand"
+	"reflect"
+	"testing/quick"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/renproject/hyperdrive/block"
+	"github.com/renproject/hyperdrive/sig"
+	"github.com/renproject/hyperdrive/sig/ecdsa"
+)
+
+var conf = quick.Config{
+	MaxCount:      256,
+	MaxCountScale: 0,
+	Rand:          nil,
+	Values:        nil,
+}
+
+var _ = Describe("Polka Builder", func() {
+	Context("when given nothing", func() {
+		It("should return false", func() {
+			builder := make(PolkaBuilder)
+
+			polka, found := builder.Polka(5)
+
+			Expect(polka.String()).To(Equal(Polka{}.String()))
+			Expect(found).To(Equal(false))
+		})
+	})
+	Context("when given a set of semi-random PreVote", func() {
+		It("should always return the most relevant Polka", func() {
+			test := func(mock mockPreVotes) bool {
+
+				builder := make(PolkaBuilder)
+
+				for _, signedPreVote := range mock.votes {
+					builder.Insert(signedPreVote)
+				}
+
+				polka, found := builder.Polka(mock.consensusThreshold)
+
+				Expect(polka.String()).To(Equal(mock.expectedPolka.String()),
+					"input PreVotes: %v\nthreshold: %v\nexpectedFound: %v\nbuilder map: %v\nfound: %v",
+					mock.votes,
+					mock.consensusThreshold,
+					mock.expectedFound,
+					builder,
+					found)
+				Expect(found).To(Equal(mock.expectedFound))
+
+				return true
+			}
+			Expect(quick.Check(test, &conf)).ShouldNot(HaveOccurred())
+		})
+	})
+})
+
+type mockPreVotes struct {
+	votes              []SignedPreVote
+	expectedPolka      Polka
+	expectedFound      bool
+	consensusThreshold int64
+}
+
+func (mockPreVotes) Generate(rand *rand.Rand, size int) reflect.Value {
+	headersSeed := make([]sig.Hash, (rand.Intn(10) + 1))
+	for i := range headersSeed {
+		val, err := quick.Value(reflect.TypeOf(sig.Hash{}), rand)
+		if !err {
+			panic("reflect failed")
+		}
+		headersSeed[i] = val.Interface().(sig.Hash)
+	}
+
+	numPreVotes := rand.Uint32() % 100
+	consensusThreshold := ((numPreVotes * 2) / 3)
+
+	signedPreVotes := make([]SignedPreVote, numPreVotes)
+
+	heighestPolka := Polka{}
+	found := false
+
+	scratch := make(map[Height]map[sig.Hash]uint32)
+
+	for i := uint32(0); i < numPreVotes; i++ {
+
+		randHashIndex := rand.Intn(len(headersSeed))
+		hash := headersSeed[randHashIndex]
+		height := Height(randHashIndex)
+
+		if val, ok := scratch[height]; ok {
+			if _, ok := val[hash]; !ok {
+				val[hash] = 1
+			} else {
+				val[hash]++
+			}
+		} else {
+			scratch[height] = make(map[sig.Hash]uint32)
+			scratch[height][hash] = 1
+		}
+
+		blk := Genesis()
+		blk.Height = height
+		blk.Header = hash
+		preVote := NewPreVote(&blk, 0, height)
+
+		newSV, err := ecdsa.NewFromRandom()
+		if err != nil {
+			panic("ecdsa failed")
+		}
+		signed, err := preVote.Sign(newSV)
+		if err != nil {
+			panic("sign failed")
+		}
+
+		signedPreVotes[i] = signed
+
+		if scratch[height][hash] >= consensusThreshold &&
+			(heighestPolka.Height < height || !found) {
+			heighestPolka = Polka{
+				Block:  &blk,
+				Round:  0,
+				Height: height,
+			}
+			found = true
+		}
+	}
+
+	return reflect.ValueOf(mockPreVotes{
+		votes:              signedPreVotes,
+		expectedPolka:      heighestPolka,
+		expectedFound:      found,
+		consensusThreshold: int64(consensusThreshold),
+	})
+}
