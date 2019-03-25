d0721556d5f298ff69ba2f135c6c26cf0cf5821a
Divya Mary <divyakoshy05@gmail.com>
2019-03-25 12:22:10 +1100

    tx: implemented FIFOPool

--- tx/pool.go
+++ tx/pool.go
@@ -1,22 +1,46 @@
 package tx
 
+import (
+	"sync"
+)
+
 type Pool interface {
 	Enqueue(Transaction)
 	Dequeue() (Transaction, bool)
 }
 
 type fifoPool struct {
+	txsMu *sync.Mutex
+	txs   Transactions
 }
 
+// FIFOPool is a First-In, First-Out transaction pool that is thread safe.
 func FIFOPool() Pool {
-	return &fifoPool{}
+	return &fifoPool{
+		txsMu: new(sync.Mutex),
+		txs:   Transactions{},
+	}
 }
 
 func (pool *fifoPool) Enqueue(tx Transaction) {
-	// TODO: Implement.
+	pool.txsMu.Lock()
+	defer pool.txsMu.Unlock()
+
+	pool.txs = append(pool.txs, tx)
 }
 
 func (pool *fifoPool) Dequeue() (Transaction, bool) {
-	// TODO: Implement.
+	pool.txsMu.Lock()
+	defer pool.txsMu.Unlock()
+
+	if len(pool.txs) > 0 {
+		tx := pool.txs[0]
+		if len(pool.txs) > 1 {
+			pool.txs = pool.txs[1:]
+			return tx, true
+		}
+		pool.txs = Transactions{}
+		return tx, true
+	}
 	return Transaction{}, false
 }
