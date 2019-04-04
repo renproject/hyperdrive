7efda05c4fc37909b8b621b328020375cd95667b
Loong <loong@republicprotocol.com>
2019-04-04 12:46:59 +0800

    replica: cache signature checks

--- replica/validator.go
+++ replica/validator.go
@@ -1,6 +1,7 @@
 package replica
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/renproject/hyperdrive/block"
@@ -43,11 +44,19 @@ type validator struct {
 	signer     sig.Verifier
 	shard      shard.Shard
 	blockchain *block.Blockchain
+
+	verifiedSignatureCache map[string]sig.Signatory
 }
 
 // NewValidator returns a Validator
 func NewValidator(signer sig.Verifier, shard shard.Shard, blockchain *block.Blockchain) Validator {
-	return &validator{signer, shard, blockchain}
+	return &validator{
+		signer:     signer,
+		shard:      shard,
+		blockchain: blockchain,
+
+		verifiedSignatureCache: map[string]sig.Signatory{},
+	}
 }
 
 func (validator *validator) ValidateBlock(signedBlock block.SignedBlock) bool {
@@ -61,22 +70,27 @@ func (validator *validator) ValidateBlock(signedBlock block.SignedBlock) bool {
 		return false
 	}
 
-	// Verify the signatory of the signedBlock
+	// TODO: Verify the Block header equals the expected header.
+
+	// Verify the parent block
+	parent, ok := validator.blockchain.Head()
+	if !ok || !parent.Header.Equal(signedBlock.ParentHeader) {
+		return false
+	}
+
+	// TODO: Check cache
+
+	// Verify the signature
 	if !validator.verifySignature(signedBlock.Block.Header, signedBlock.Signature, signedBlock.Signatory) {
 		return false
 	}
 
-	// Is block.ParentHeader (at H) == block.Header (at H-1)?
-	parent, ok := validator.blockchain.Head()
-	if !ok {
-		return false
-	}
-
-	return parent.Header.Equal(signedBlock.ParentHeader)
+	// TODO: Fill cache
+	return true
 }
 
 func (validator *validator) ValidatePreVote(preVote block.SignedPreVote) bool {
-	// 1. Verify the pre-vote is well-formed
+	// Verify the pre-vote is well-formed
 	if preVote.PreVote.Block != nil {
 		if !validator.ValidateBlock(*preVote.PreVote.Block) {
 			return false
@@ -92,26 +106,42 @@ func (validator *validator) ValidatePreVote(preVote block.SignedPreVote) bool {
 		return false
 	}
 
-	// 2. Verify the signatory of the pre-vote
+	// Compute the PreVote hash
 	hashSum256 := sha3.Sum256([]byte(preVote.PreVote.String()))
 	hash := sig.Hash{}
 	copy(hash[:], hashSum256[:])
 
-	return validator.verifySignature(hash, preVote.Signature, preVote.Signatory)
+	// TODO: Check cache
+
+	// Verify the signature
+	if !validator.verifySignature(hash, preVote.Signature, preVote.Signatory) {
+		return false
+	}
+
+	// TODO: Fill cache
+	return true
 }
 
 func (validator *validator) ValidatePreCommit(preCommit block.SignedPreCommit) bool {
-	// 1. Verify the pre-commit is well-formed
+	// Verify the underlying Polka is well-formed
 	if !validator.ValidatePolka(preCommit.PreCommit.Polka) {
 		return false
 	}
 
-	// 2. Verify the signatory of the pre-commit
+	// Compute the PreCommit hash
 	hashSum256 := sha3.Sum256([]byte(preCommit.PreCommit.String()))
 	hash := sig.Hash{}
 	copy(hash[:], hashSum256[:])
 
-	return validator.verifySignature(hash, preCommit.Signature, preCommit.Signatory)
+	// TODO: Check cache
+
+	// Verify the signature
+	if !validator.verifySignature(hash, preCommit.Signature, preCommit.Signatory) {
+		return false
+	}
+
+	// TODO: Fill cache
+	return true
 }
 
 func (validator *validator) ValidatePolka(polka block.Polka) bool {
@@ -141,7 +171,15 @@ func (validator *validator) ValidatePolka(polka block.Polka) bool {
 	}
 	data := []byte(preVote.String())
 
-	return validator.verifySignatures(data, polka.Signatures, polka.Signatories)
+	// TODO: Check cache
+
+	// Verify the signature
+	if !validator.verifySignatures(data, polka.Signatures, polka.Signatories) {
+		return false
+	}
+
+	// TODO: Fill cache
+	return true
 }
 
 // TODO: Un-comment when needed
@@ -154,28 +192,29 @@ func (validator *validator) ValidatePolka(polka block.Polka) bool {
 // 	return validator.ValidatePolka(commit.Polka) && validator.verifySignatures(data, commit.Signatures, commit.Signatories)
 // }
 
-// isSignatoryInShard returns true if the given signatory belongs to the
-// provided shard.
-func (validator *validator) isSignatoryInShard(signatory sig.Signatory) bool {
-	for _, sig := range validator.shard.Signatories {
-		if signatory.Equal(sig) {
-			return true
-		}
-	}
-	return false
-}
-
 // verifySignature verifies that the signatory provided was used to generate
 // the signature for the given hash. Also verifies that the signatory is a
 // part of the given shard.
 func (validator *validator) verifySignature(hash sig.Hash, signature sig.Signature, signatory sig.Signatory) bool {
-	verifiedSig, err := validator.signer.Verify(hash, signature)
-	if err != nil || !verifiedSig.Equal(signatory) {
-		// TODO: log the error
+	cachedHashAndSignature := fmt.Sprintf("%v:%v", hash, signature)
+	if cachedSignatory, ok := validator.verifiedSignatureCache[cachedHashAndSignature]; ok {
+		if cachedSignatory.Equal(signatory) {
+			return true
+		}
+	}
+
+	verifiedSignatory, err := validator.signer.Verify(hash, signature)
+	if err != nil || !verifiedSignatory.Equal(signatory) {
+		// FIXME: Do we need to log the error?
 		return false
 	}
 
-	return validator.isSignatoryInShard(verifiedSig)
+	if !validator.isSignatoryInShard(verifiedSignatory) {
+		return false
+	}
+
+	validator.verifiedSignatureCache[cachedHashAndSignature] = verifiedSignatory
+	return true
 }
 
 func (validator *validator) verifySignatures(data []byte, signatures sig.Signatures, signatories sig.Signatories) bool {
@@ -195,4 +234,15 @@ func (validator *validator) verifySignatures(data []byte, signatures sig.Signatu
 		}
 	}
 	return true
+}
+
+// isSignatoryInShard returns true if the given signatory belongs to the
+// provided shard.
+func (validator *validator) isSignatoryInShard(signatory sig.Signatory) bool {
+	for _, sig := range validator.shard.Signatories {
+		if signatory.Equal(sig) {
+			return true
+		}
+	}
+	return false
 }
