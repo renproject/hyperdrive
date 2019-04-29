babe453d8beee81dcbf8c69d12e72cdffbb09277
Divya Mary <divyakoshy05@gmail.com>
2019-04-29 16:05:51 +1000

    updated Transaction interface

--- tx/tx.go
+++ tx/tx.go
@@ -1,7 +1,28 @@
 package tx
 
-type Transaction struct {
-	Data [32]byte
+type Transaction interface {
+	IsTransaction()
+	Marshal() ([]byte, error)
+	Unmarshal(data []byte) error
 }
 
 type Transactions []Transaction
+
+type transaction struct {
+	Data [32]byte
+}
+
+func NewTransaction(data [32]byte) Transaction {
+	return &transaction{data}
+}
+
+func (transaction) IsTransaction() {}
+
+func (transaction *transaction) Marshal() ([]byte, error) {
+	return transaction.Data[:], nil
+}
+
+func (transaction *transaction) Unmarshal(data []byte) error {
+	copy(transaction.Data[:], data)
+	return nil
+}
