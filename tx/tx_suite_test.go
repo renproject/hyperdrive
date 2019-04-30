ed6f30f6d35019ae73f2b442f40dc44b8459b424
Divya Mary <divyakoshy05@gmail.com>
2019-04-30 11:32:32 +1000

    tx: added tests for txPool and txs

--- tx/tx_suite_test.go
+++ tx/tx_suite_test.go
@@ -1,1 +1,13 @@
 package tx_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestTx(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Tx Suite")
+}
