966f7139da92bcacbb81024ca346369d442b22bb
Divya Mary <divyakoshy05@gmail.com>
2019-04-30 10:45:23 +1000

    replica: fix failing tests; limit txs to block.MaxTransactions

--- replica/replica.go
+++ replica/replica.go
@@ -194,7 +194,7 @@ func (replica *replica) buildSignedBlock() block.SignedBlock {
 	// TODO: We should put more than one transaction into a block.
 	transactions := tx.Transactions{}
 	transaction, ok := replica.txPool.Dequeue()
-	for ok && len(transactions) < 10 { // TODO: Make this limit configurable
+	for ok && len(transactions) < block.MaxTransactions {
 		transactions = append(transactions, transaction)
 		transaction, ok = replica.txPool.Dequeue()
 	}
