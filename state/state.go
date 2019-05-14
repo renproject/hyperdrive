8763a7c9254fe130b20be87048997b297927b0d0
Loong <loong@republicprotocol.com>
2019-05-14 18:56:52 +0800

    state: implement round locking in state machine

--- state/state.go
+++ state/state.go
@@ -1,68 +1,20 @@
 package state
 
-import (
-	"github.com/renproject/hyperdrive/block"
-)
-
 type State interface {
-	Round() block.Round
-	Height() block.Height
+	IsState()
 }
 
 type WaitingForPropose struct {
-	round  block.Round
-	height block.Height
 }
 
-func WaitForPropose(round block.Round, height block.Height) State {
-	return WaitingForPropose{
-		round:  round,
-		height: height,
-	}
-}
-
-func (waitingForPropose WaitingForPropose) Round() block.Round {
-	return waitingForPropose.round
-}
-
-func (waitingForPropose WaitingForPropose) Height() block.Height {
-	return waitingForPropose.height
-}
+func (WaitingForPropose) IsState() {}
 
 type WaitingForPolka struct {
-	round  block.Round
-	height block.Height
 }
 
-func WaitForPolka(round block.Round, height block.Height) State {
-	return WaitingForPolka{
-		round:  round,
-		height: height,
-	}
-}
-
-func (waitingForPolka WaitingForPolka) Round() block.Round {
-	return waitingForPolka.round
-}
-
-func (waitingForPolka WaitingForPolka) Height() block.Height {
-	return waitingForPolka.height
-}
+func (WaitingForPolka) IsState() {}
 
 type WaitingForCommit struct {
-	polka block.Polka
 }
 
-func WaitForCommit(polka block.Polka) State {
-	return WaitingForCommit{
-		polka: polka,
-	}
-}
-
-func (waitingForCommit WaitingForCommit) Round() block.Round {
-	return waitingForCommit.polka.Round
-}
-
-func (waitingForCommit WaitingForCommit) Height() block.Height {
-	return waitingForCommit.polka.Height
-}
+func (WaitingForCommit) IsState() {}
