36ea406212fb47cde5dc7336df37f4013d1b1d35
Loong <loong@republicprotocol.com>
2019-05-16 16:05:03 +0800

    state: add marker tests for marker interfaces

--- state/state_test.go
+++ state/state_test.go
@@ -1,1 +1,26 @@
 package state_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	"github.com/renproject/hyperdrive/state"
+)
+
+var _ = Describe("States", func() {
+	Context("when using WaitingForPropose", func() {
+		It("should implement the State interface", func() {
+			state.WaitingForPropose{}.IsState()
+		})
+	})
+
+	Context("when using WaitingForPolka", func() {
+		It("should implement the State interface", func() {
+			state.WaitingForPolka{}.IsState()
+		})
+	})
+
+	Context("when using WaitingForCommit", func() {
+		It("should implement the State interface", func() {
+			state.WaitingForCommit{}.IsState()
+		})
+	})
+})
