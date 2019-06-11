dd37b40c79a489e2c9db7e2b661fb37cf5fc36fe
Divya Mary <divyakoshy05@gmail.com>
2019-06-11 14:57:57 +1000

    replcia: add more validator tests

--- replica/validator_test.go
+++ replica/validator_test.go
@@ -47,4 +47,36 @@ var _ = Describe("Validator", func() {
 			Expect(validator.ValidatePropose(newPropose)).To(BeTrue())
 		})
 	})
+
+	Context("when an invalid commit is provided", func() {
+		It("should return false", func() {
+			signer1, err := ecdsa.NewFromRandom()
+			Expect(err).ShouldNot(HaveOccurred())
+
+			signer2, err := ecdsa.NewFromRandom()
+			Expect(err).ShouldNot(HaveOccurred())
+
+			shard := shard.Shard{
+				Hash:        sig.Hash{},
+				BlockHeader: sig.Hash{},
+				BlockHeight: 0,
+				Signatories: sig.Signatories{signer1.Signatory(), signer2.Signatory()},
+			}
+			validator := NewValidator(signer1, shard)
+
+			parent := testutils.SignBlock(block.Block{Time: time.Now(), Height: 1}, signer1)
+			precommit1 := testutils.GenerateSignedPreCommit(*parent, signer1, []sig.SignerVerifier{signer1, signer2})
+			commit := block.Commit{
+				Polka:       precommit1.Polka,
+				Signatures:  sig.Signatures{precommit1.Signature, testutils.RandomSignature()},
+				Signatories: sig.Signatories{precommit1.Signatory, testutils.RandomSignatory()},
+			}
+			Expect(validator.ValidateCommit(commit)).To(BeFalse())
+
+			newBlock := testutils.SignBlock(block.Block{Time: time.Now(), Height: 2, ParentHeader: parent.Header}, signer1)
+			newPropose := testutils.GenerateSignedPropose(*newBlock, 0, signer1)
+			newPropose.LastCommit = &commit
+			Expect(validator.ValidatePropose(newPropose)).To(BeFalse())
+		})
+	})
 })
