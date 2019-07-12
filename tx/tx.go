2f0df74b0ba3e9da54219580e197e65672afa496
Loong <loong@renproject.io>
2019-07-12 18:21:06 +0800

    tx: use read/write pattern for marshaling

--- tx/tx.go
+++ tx/tx.go
@@ -1,66 +1,53 @@
 package tx
 
 import (
-	"bytes"
 	"encoding/binary"
+	"io"
 )
 
 type Transaction []byte
 
-func (tx Transaction) MarshalBinary() ([]byte, error) {
-	buf := new(bytes.Buffer)
-	if err := binary.Write(buf, binary.LittleEndian, uint64(len(tx))); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(buf, binary.LittleEndian, tx); err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
-}
-
-func (tx *Transaction) UnmarshalBinary(data []byte) error {
-	buf := bytes.NewBuffer(data)
-
-	var n uint64
-	if err := binary.Read(buf, binary.LittleEndian, &n); err != nil {
+func (tx Transaction) Write(w io.Writer) error {
+	if err := binary.Write(w, binary.LittleEndian, uint64(len(tx))); err != nil {
 		return err
 	}
+	_, err := w.Write(tx[:])
+	return err
+}
 
-	_, err := buf.Read(*tx)
-
+func (tx *Transaction) Read(r io.Reader) error {
+	var n uint64
+	if err := binary.Read(r, binary.LittleEndian, &n); err != nil {
+		return err
+	}
+	*tx = make(Transaction, n)
+	_, err := r.Read(*tx)
 	return err
 }
 
 type Transactions []Transaction
 
-func (txs Transactions) MarshalBinary() ([]byte, error) {
-	buf := new(bytes.Buffer)
-	if err := binary.Write(buf, binary.LittleEndian, uint64(len(txs))); err != nil {
-		return nil, err
+func (txs Transactions) Write(w io.Writer) error {
+	if err := binary.Write(w, binary.LittleEndian, uint64(len(txs))); err != nil {
+		return err
 	}
-	for _, tx := range txs {
-		data, err := tx.MarshalBinary()
-		if err != nil {
-			return nil, err
-		}
-		if err := binary.Write(buf, binary.LittleEndian, data); err != nil {
-			return nil, err
+	for _, sig := range txs {
+		if err := sig.Write(w); err != nil {
+			return err
 		}
 	}
-	return buf.Bytes(), nil
+	return nil
 }
 
-func (txs *Transactions) UnmarshalBinary(data []byte) error {
-	buf := bytes.NewBuffer(data)
-
+func (txs *Transactions) Read(r io.Reader) error {
 	var n uint64
-	if err := binary.Read(buf, binary.LittleEndian, &n); err != nil {
+	if err := binary.Read(r, binary.LittleEndian, &n); err != nil {
 		return err
 	}
 
 	*txs = make(Transactions, n)
 	for i := range *txs {
-		if err := (*txs)[i].UnmarshalBinary(buf.Bytes()); err != nil {
+		if err := (*txs)[i].Read(r); err != nil {
 			return err
 		}
 	}
