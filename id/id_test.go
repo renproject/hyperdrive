82958f54774948c68b6ac3b0ada4ce3e8aa2b01b
Loong <loong@renproject.io>
2019-07-31 10:48:35 +0800

    id: def test suite

--- id/id_test.go
+++ id/id_test.go
@@ -6,9 +6,143 @@ import (
 )
 
 var _ = Describe("ID", func() {
-	Context("when implementing the test suite needs to be done", func() {
-		It("should always fail the test", func() {
-			Expect(true).To(BeFalse())
+	Context("when stringifying", func() {
+		Context("when stringifying random hashes", func() {
+			Context("when hashes are equal", func() {
+				It("should return equal strings", func() {
+					Expect(true).To(BeFalse())
+				})
+			})
+
+			Context("when hashes are unequal", func() {
+				It("should return unequal strings", func() {
+					Expect(true).To(BeFalse())
+				})
+			})
+		})
+
+		Context("when stringifying random signatures", func() {
+			Context("when signatures are equal", func() {
+				It("should return equal strings", func() {
+					Expect(true).To(BeFalse())
+				})
+			})
+
+			Context("when signatures are unequal", func() {
+				It("should return unequal strings", func() {
+					Expect(true).To(BeFalse())
+				})
+			})
+		})
+
+		Context("when stringifying random signatories", func() {
+			Context("when signatories are equal", func() {
+				It("should return equal strings", func() {
+					Expect(true).To(BeFalse())
+				})
+			})
+
+			Context("when signatories are unequal", func() {
+				It("should return unequal strings", func() {
+					Expect(true).To(BeFalse())
+				})
+			})
+		})
+	})
+
+	Context("when comparing", func() {
+		Context("when comparing random hashes", func() {
+			Context("when hashes are equal", func() {
+				It("should return true", func() {
+					Expect(true).To(BeFalse())
+				})
+			})
+
+			Context("when hashes are unequal", func() {
+				It("should return false", func() {
+					Expect(true).To(BeFalse())
+				})
+			})
+		})
+
+		Context("when comparing random signatures", func() {
+			Context("when signatures are equal", func() {
+				It("should return true", func() {
+					Expect(true).To(BeFalse())
+				})
+			})
+
+			Context("when signatures are unequal", func() {
+				It("should return false", func() {
+					Expect(true).To(BeFalse())
+				})
+			})
+		})
+
+		Context("when comparing random signatories", func() {
+			Context("when signatories are equal", func() {
+				It("should return true", func() {
+					Expect(true).To(BeFalse())
+				})
+			})
+
+			Context("when signatories are unequal", func() {
+				It("should return false", func() {
+					Expect(true).To(BeFalse())
+				})
+			})
+		})
+	})
+
+	Context("when marshaling", func() {
+		Context("when marshaling a random hash", func() {
+			It("should equal itself after marshaling and then unmarshaling", func() {
+				Expect(true).To(BeFalse())
+			})
+		})
+
+		Context("when marshaling random hashes", func() {
+			It("should equal itself after marshaling and then unmarshaling", func() {
+				Expect(true).To(BeFalse())
+			})
+		})
+
+		Context("when marshaling a random signature", func() {
+			It("should equal itself after marshaling and then unmarshaling", func() {
+				Expect(true).To(BeFalse())
+			})
+		})
+
+		Context("when marshaling random signatures", func() {
+			It("should equal itself after marshaling and then unmarshaling", func() {
+				Expect(true).To(BeFalse())
+			})
+		})
+
+		Context("when marshaling a random signatory", func() {
+			It("should equal itself after marshaling and then unmarshaling", func() {
+				Expect(true).To(BeFalse())
+			})
+		})
+
+		Context("when marshaling random signatories", func() {
+			It("should equal itself after marshaling and then unmarshaling", func() {
+				Expect(true).To(BeFalse())
+			})
+		})
+	})
+
+	Context("when creating signatories", func() {
+		Context("when using pubkeys that are equal", func() {
+			It("should return signatories that are equal", func() {
+				Expect(true).To(BeFalse())
+			})
+		})
+
+		Context("when using pubkeys that are unequal", func() {
+			It("should return signatories that are unequal", func() {
+				Expect(true).To(BeFalse())
+			})
 		})
 	})
 })
