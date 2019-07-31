698f71c32a7ce2fd67e1667003a32c4c968c6d15
Loong <loong@renproject.io>
2019-07-31 11:43:04 +0800

    process: def state test suite

--- process/state_test.go
+++ process/state_test.go
@@ -1,1 +1,34 @@
 package process_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("States", func() {
+	Context("when marshaling", func() {
+		Context("when marshaling a random state", func() {
+			It("should equal itself after marshaling and then unmarshaling", func() {
+				Expect(true).To(BeFalse())
+			})
+		})
+	})
+
+	Context("when resetting equal states", func() {
+		It("should return states that are equal", func() {
+			Expect(true).To(BeFalse())
+		})
+	})
+
+	Context("when resetting unequal states", func() {
+		It("should return states that are equal", func() {
+			Expect(true).To(BeFalse())
+		})
+	})
+
+	Context("when resetting the default states", func() {
+		It("should return a state that is equal to the default state", func() {
+			Expect(true).To(BeFalse())
+		})
+	})
+})
