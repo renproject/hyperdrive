fd1f3d94818d8562bd6d2b9618eddbc3ef0b2411
Jaz Gulati <jaskaran.g15@gmail.com>
2020-03-06 10:55:01 +1100

    process, replica: handle resync message in marshalers

--- process/marshal.go
+++ process/marshal.go
@@ -558,6 +558,10 @@ func (inbox *Inbox) UnmarshalJSON(data []byte) error {
 					msg := new(Precommit)
 					err = json.Unmarshal(raw, msg)
 					inbox.messages[height][round][sig] = msg
+				case ResyncMessageType:
+					msg := new(Resync)
+					err = json.Unmarshal(raw, msg)
+					inbox.messages[height][round][sig] = msg
 				}
 				if err != nil {
 					return err
@@ -673,6 +677,10 @@ func (inbox *Inbox) UnmarshalBinary(data []byte) error {
 					sigMap[sig] = message
 				case PrecommitMessageType:
 					message := new(Precommit)
+					err = message.UnmarshalBinary(messageBytes)
+					sigMap[sig] = message
+				case ResyncMessageType:
+					message := new(Resync)
 					err = message.UnmarshalBinary(messageBytes)
 					sigMap[sig] = message
 				}
