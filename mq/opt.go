54ebcdfb5c30cfc6f4746a3ec45da78b93910a9b
Rohit Narurkar <rohit.narurkar@protonmail.com>
2020-07-08 14:56:16 +0530

    config for github actions

--- mq/opt.go
+++ mq/opt.go
@@ -1,12 +1,21 @@
 package mq
 
-import "github.com/sirupsen/logrus"
+import "go.uber.org/zap"
 
+// Options define the Message Queue options
 type Options struct {
-	Logger      logrus.FieldLogger
+	Logger      *zap.Logger
 	MaxCapacity int
 }
 
+// DefaultOptions returns the default options as used by the Message Queue
 func DefaultOptions() Options {
-	return Options{}
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	return Options{
+		Logger:      logger,
+		MaxCapacity: 1000,
+	}
 }
