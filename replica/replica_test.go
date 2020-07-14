d58c58c293d618ab99577e58d8773b72291e8660
Rohit Narurkar <rohit.narurkar@protonmail.com>
2020-07-14 16:27:12 +0530

    testing stalled progress in case of fewer than required online replicas

--- replica/replica_test.go
+++ replica/replica_test.go
@@ -222,8 +222,8 @@ var _ = Describe("Replica", func() {
 
 			// setup private keys for the replicas
 			// and their signatories
-			privKeys := make([]*id.PrivKey, n)
-			signatories := make([]id.Signatory, n)
+			privKeys := make([]*id.PrivKey, 3*f+1)
+			signatories := make([]id.Signatory, 3*f+1)
 			for i := range privKeys {
 				privKeys[i] = id.NewPrivKey()
 				signatories[i] = privKeys[i].Signatory()
@@ -1057,6 +1057,387 @@ var _ = Describe("Replica", func() {
 					}
 				}
 			})
+		})
+	})
+
+	Context("with less than 2f+1 replicas online", func() {
+		It("should stall consensus and should not progress", func() {
+			// randomness seed
+			rSeed := time.Now().UnixNano()
+			r := rand.New(rand.NewSource(rSeed))
+
+			// f is the maximum no. of adversaries
+			// n is the number of honest replicas online
+			// h is the target minimum consensus height
+			f := 3
+			n := 2 * f
+			targetHeight := process.Height(2)
+
+			// commits from replicas
+			commits := make(map[int]map[process.Height]process.Value)
+
+			// setup private keys for the replicas
+			// and their signatories
+			privKeys := make([]*id.PrivKey, 3*f+1)
+			signatories := make([]id.Signatory, 3*f+1)
+			for i := range privKeys {
+				privKeys[i] = id.NewPrivKey()
+				signatories[i] = privKeys[i].Signatory()
+				commits[i] = make(map[process.Height]process.Value)
+			}
+
+			// every replica sends this signal when they reach the target
+			// consensus height
+			completionSignal := make(chan bool, n)
+
+			// slice of messages to be broadcasted between replicas.
+			// messages from mq are popped and handled whenever mqSignal is signalled
+			mq := []Message{}
+			mqSignal := make(chan struct{}, n*n)
+			mqSignal <- struct{}{}
+
+			// build replicas
+			replicas := make([]*replica.Replica, n)
+			for i := range replicas {
+				replicaIndex := i
+
+				replicas[i] = replica.New(
+					replica.DefaultOptions(),
+					signatories[i],
+					signatories,
+					// Proposer
+					processutil.MockProposer{
+						MockValue: func() process.Value {
+							v := processutil.RandomGoodValue(r)
+							return v
+						},
+					},
+					// Validator
+					processutil.MockValidator{
+						MockValid: func(process.Value) bool {
+							return true
+						},
+					},
+					// Committer
+					processutil.CommitterCallback{
+						Callback: func(height process.Height, value process.Value) {
+							// add commit to the commits map
+							commits[replicaIndex][height] = value
+							// signal for completion if this is the target height
+							if height == targetHeight {
+								completionSignal <- true
+							}
+						},
+					},
+					// Catcher
+					nil,
+					// Broadcaster
+					processutil.BroadcasterCallbacks{
+						BroadcastProposeCallback: func(propose process.Propose) {
+							for j := 0; j < n; j++ {
+								mq = append(mq, Message{
+									to:    j,
+									value: propose,
+								})
+							}
+						},
+						BroadcastPrevoteCallback: func(prevote process.Prevote) {
+							for j := 0; j < n; j++ {
+								mq = append(mq, Message{
+									to:    j,
+									value: prevote,
+								})
+							}
+						},
+						BroadcastPrecommitCallback: func(precommit process.Precommit) {
+							for j := 0; j < n; j++ {
+								mq = append(mq, Message{
+									to:    j,
+									value: precommit,
+								})
+							}
+						},
+					},
+					// Flusher
+					func() {
+						mqSignal <- struct{}{}
+					},
+				)
+			}
+
+			// Create a global context that can be used to cancel the running of all
+			// replicas.
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			// we expect the consensus to stall, and we wait for the said time
+			// before declaring that the expected behaviour was observed
+			successTestSignal := make(chan bool, 1)
+			go func() {
+				time.Sleep(10 * time.Second)
+				successTestSignal <- true
+			}()
+
+			// From the global context, create per-replica contexts that can be used to
+			// cancel replicas independently of one another. This is useful when we want
+			// to simulate replicas "crashing" and "restarting" during consensus.
+			replicaCtxs, replicaCtxCancels := make([]context.Context, n), make([]context.CancelFunc, n)
+			for i := range replicaCtxs {
+				replicaCtxs[i], replicaCtxCancels[i] = context.WithCancel(ctx)
+			}
+
+			// Run all of the replicas in independent background goroutines.
+			for i := range replicas {
+				go replicas[i].Run(replicaCtxs[i])
+			}
+
+			time.Sleep(100 * time.Millisecond)
+			isRunning := true
+			for isRunning {
+				select {
+				case _ = <-successTestSignal:
+					isRunning = false
+					break
+				case _ = <-mqSignal:
+					// this means the target consensus has been reached on every replica
+					if len(completionSignal) == n {
+						panic("consensus should not have progressed so far")
+					}
+
+					if len(mq) == 0 {
+						continue
+					}
+
+					// pop the first message
+					m := mq[0]
+					mq = mq[1:]
+
+					// handle the message
+					replica := replicas[m.to]
+					switch value := m.value.(type) {
+					case process.Propose:
+						replica.Propose(context.Background(), value)
+					case process.Prevote:
+						replica.Prevote(context.Background(), value)
+					case process.Precommit:
+						replica.Precommit(context.Background(), value)
+					default:
+						panic(fmt.Errorf("non-exhaustive pattern: message.value has type %T", value))
+					}
+				}
+			}
+		})
+	})
+
+	Context("with 2f+1 replicas online, and one of them goes offline", func() {
+		It("should stall consensus as soon as there are less than 2f+1 online", func() {
+			// randomness seed
+			rSeed := time.Now().UnixNano()
+			r := rand.New(rand.NewSource(rSeed))
+
+			// f is the maximum no. of adversaries
+			// n is the number of honest replicas online
+			// h is the target minimum consensus height
+			f := 3
+			n := 2*f + 1
+			intermTargetHeight := process.Height(6)
+			finalTargetHeight := process.Height(8)
+
+			// commits from replicas
+			commits := make(map[int]map[process.Height]process.Value)
+
+			// setup private keys for the replicas
+			// and their signatories
+			privKeys := make([]*id.PrivKey, 3*f+1)
+			signatories := make([]id.Signatory, 3*f+1)
+			for i := range privKeys {
+				privKeys[i] = id.NewPrivKey()
+				signatories[i] = privKeys[i].Signatory()
+				commits[i] = make(map[process.Height]process.Value)
+			}
+
+			// every replica sends this signal when they reach the target
+			// consensus height
+			completionSignal := make(chan bool, n)
+
+			// replicas randomly send this signal that kills them
+			// this is to simulate a behaviour where f replicas go offline
+			killedReplicas := make(map[int]bool)
+			killSignal := make(chan int, f)
+
+			// slice of messages to be broadcasted between replicas.
+			// messages from mq are popped and handled whenever mqSignal is signalled
+			mq := []Message{}
+			mqSignal := make(chan struct{}, n*n)
+			mqSignal <- struct{}{}
+
+			// build replicas
+			replicas := make([]*replica.Replica, n)
+			for i := range replicas {
+				replicaIndex := i
+
+				replicas[i] = replica.New(
+					replica.DefaultOptions().
+						WithTimerOptions(
+							timer.DefaultOptions().
+								WithTimeout(1*time.Second),
+						),
+					signatories[i],
+					signatories,
+					// Proposer
+					processutil.MockProposer{
+						MockValue: func() process.Value {
+							v := processutil.RandomGoodValue(r)
+							return v
+						},
+					},
+					// Validator
+					processutil.MockValidator{
+						MockValid: func(process.Value) bool {
+							return true
+						},
+					},
+					// Committer
+					processutil.CommitterCallback{
+						Callback: func(height process.Height, value process.Value) {
+							// add commit to the commits map
+							commits[replicaIndex][height] = value
+
+							// signal for completion if this is the target height
+							if height == finalTargetHeight {
+								completionSignal <- true
+								return
+							}
+
+							if height == intermTargetHeight {
+								killSignal <- 0
+								return
+							}
+						},
+					},
+					// Catcher
+					nil,
+					// Broadcaster
+					processutil.BroadcasterCallbacks{
+						BroadcastProposeCallback: func(propose process.Propose) {
+							for j := 0; j < n; j++ {
+								if _, ok := killedReplicas[j]; !ok {
+									mq = append(mq, Message{
+										to:    j,
+										value: propose,
+									})
+								}
+							}
+						},
+						BroadcastPrevoteCallback: func(prevote process.Prevote) {
+							for j := 0; j < n; j++ {
+								if _, ok := killedReplicas[j]; !ok {
+									mq = append(mq, Message{
+										to:    j,
+										value: prevote,
+									})
+								}
+							}
+						},
+						BroadcastPrecommitCallback: func(precommit process.Precommit) {
+							for j := 0; j < n; j++ {
+								if _, ok := killedReplicas[j]; !ok {
+									mq = append(mq, Message{
+										to:    j,
+										value: precommit,
+									})
+								}
+							}
+						},
+					},
+					// Flusher
+					func() {
+						mqSignal <- struct{}{}
+					},
+				)
+			}
+
+			// Create a global context that can be used to cancel the running of all
+			// replicas.
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			// in case the test fails to proceed to completion, we have a signal
+			// to mark it as failed
+			// this test should take 30 seconds to complete
+			successTestSignal := make(chan bool, 1)
+			go func() {
+				time.Sleep(20 * time.Second)
+				successTestSignal <- true
+			}()
+
+			// From the global context, create per-replica contexts that can be used to
+			// cancel replicas independently of one another. This is useful when we want
+			// to simulate replicas "crashing" and "restarting" during consensus.
+			replicaCtxs, replicaCtxCancels := make([]context.Context, n), make([]context.CancelFunc, n)
+			for i := range replicaCtxs {
+				replicaCtxs[i], replicaCtxCancels[i] = context.WithCancel(ctx)
+			}
+
+			// Run all of the replicas in independent background goroutines.
+			for i := range replicas {
+				go replicas[i].Run(replicaCtxs[i])
+			}
+
+			time.Sleep(100 * time.Millisecond)
+			isRunning := true
+			for isRunning {
+				select {
+				// if the expected time frame is over, we fail the test
+				case _ = <-successTestSignal:
+					isRunning = false
+					break
+				// else continue watching out for the mqSignal
+				case _ = <-mqSignal:
+					// this means the target consensus has been reached on every replica
+					if len(completionSignal) == 2*f {
+						panic("consensus should have stalled before this")
+					}
+
+					// kill a replica if there has been a kill signal
+					if len(killSignal) > 0 {
+						killIndex := <-killSignal
+						if _, ok := killedReplicas[killIndex]; !ok {
+							replicaCtxCancels[killIndex]()
+							killedReplicas[killIndex] = true
+						}
+						continue
+					}
+
+					// ignore if there isn't any message
+					if len(mq) == 0 {
+						continue
+					}
+
+					// pop the first message
+					m := mq[0]
+					mq = mq[1:]
+
+					// ignore if its a nil message
+					if m.value == nil {
+						continue
+					}
+
+					// handle the message
+					time.Sleep(5 * time.Millisecond)
+					replica := replicas[m.to]
+					switch value := m.value.(type) {
+					case process.Propose:
+						replica.Propose(context.Background(), value)
+					case process.Prevote:
+						replica.Prevote(context.Background(), value)
+					case process.Precommit:
+						replica.Precommit(context.Background(), value)
+					default:
+						panic(fmt.Errorf("non-exhaustive pattern: message.value has type %T", value))
+					}
+				}
+			}
 		})
 	})
 })
